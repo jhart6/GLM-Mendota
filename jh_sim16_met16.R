87f7275cc13d9f4e84b9838e86d70fae85d29655
jhart6 <jhart6@wisc.edu>
2016-12-13 12:45:05 -0600

    formatted 2016 met data to run 2016 sim, now plotting 2016 sim results against observed data

--- jh_sim16_met16.R
+++ jh_sim16_met16.R
@@ -38,7 +38,103 @@ sixmile <- read.csv("Mendota_sixmile.csv", header=TRUE)
 sixmile$Time <-as.POSIXct(strptime(sixmile$Time, "%Y-%m-%d %H:%M:%S", tz="EST"))
 write.csv(sixmile, "Mendota_sixmile.csv", row.names=FALSE, quote=FALSE)
 
+met<-read.csv("NLDAS2_Mendota_2010_2016_cell_5.csv",header=TRUE)
+met$time <- as.POSIXct(strptime(met$time, "%Y-%m-%d %H:%M:%S", tz="EST"))
+write.csv(met,"NLDAS2_Mendota_2010_2016_cell_5.csv",row.names=FALSE, quote=FALSE)
+
 #run GLM
 #START: 2016-04-15 00:00:00
 #END: 2016-11-11 23:00:00
 run_glm(SimDir)
+
+####Plot GLM Results####
+#know your options of things to plot from output.nc
+vars<-sim_vars('output.nc') #sim_vars = simulation variables
+View(vars)
+
+#Convert Variables
+ConvertVariables = TRUE
+
+#Need to write some more of these
+if (ConvertVariables){
+  convert_sim_var(nc_file, DO = OXY_oxy * 32/1000, unit = 'mg/L',overwrite = T)
+  convert_sim_var(nc_file, DOC = OGM_doc * 12/1000, unit = 'mg/L',overwrite = T)
+  convert_sim_var(nc_file, POC = OGM_poc * 12/1000, unit = 'mg/L', overwrite = T)
+  convert_sim_var(nc_file, TotP2 = TOT_tp * 30.97/1000, unit = 'mg/L',overwrite = T)
+  convert_sim_var(nc_file, TotN2 = TOT_tn * 14/1000, unit = 'mg/L',overwrite = T)
+  #convert_sim_var(nc_file, Methane = log(CAR_ch4) , unit = 'umol/L', overwrite = T)
+}
+
+
+
+#plot 2016 simulation results
+quartz()
+plot_temp(file=nc_file, fig_path=FALSE) #standard plot temp function
+plot_var(file=nc_file,c('temp','evap')) #to plot two vars at once
+plot_var(file=nc_file,'DO',fig_path=FALSE) #AED vars
+plot_var(file=nc_file,'POC',fig_path=FALSE,col_lim = c(0,3)) #AED vars
+plot_var(file=nc_file, 'CAR_ch4',fig_path=FALSE) #AED vars
+plot_var(file=nc_file,'DOC',fig_path=FALSE) #AED vars
+plot_var(file=nc_file,'CAR_pCO2',fig_path=FALSE,col_lim=c(0,3))
+
+
+####Compare 16 Sim to 16 Obs####
+#import 2016 observational data
+#2016 obs data from 4/15/2016 through 11/14/2016 ##NEED TO UPDATE
+temp16<-read.csv('obs_temp16.csv')
+juliaTEMP<-paste(SimDir,'/','juliaTEMP.csv',sep="")
+write.csv(temp16,file = juliaTEMP,row.names = FALSE, quote = FALSE)
+
+do16<-read.csv('obs_DO16.csv')
+juliaDO<-paste(SimDir,'/','juliaDO.csv',sep ="")
+write.csv(do16,file = juliaDO, row.names = FALSE, quote = FALSE)
+
+poc16<-read.csv('obs_POC16.csv')
+juliaPOC<-paste(SimDir,'/','juliaPOC.csv',sep="")
+write.csv(poc16,file = juliaPOC, row.names=FALSE, quote=FALSE)
+
+ch416<-read.csv('obs_CH416.csv')
+juliaCH4<-paste(SimDir, '/', 'juliaCH4.csv',sep="")
+write.csv(ch416,file=juliaCH4, row.names=FALSE, quote=FALSE)
+
+
+methane <- read.csv("juliaCH4.csv", header=TRUE)
+methane$datetime <-as.POSIXct(strptime(methane$datetime, "%Y-%m-%d %H:%M:%S", tz="EST"))
+write.csv(methane, "juliaCH4.csv", row.names=FALSE, quote=FALSE)
+
+
+obsTEMP<-read.csv("field_temp.csv",header=TRUE)
+obsTEMP<-paste(SimDir, '/', 'obsTEMP.csv', sep="")
+write.csv(obsTEMP, 'obsTEMP.csv', row.names=FALSE, quote=FALSE)
+
+obsDO<-read.csv("field_do.csv",header=TRUE)
+obsPH<-read.csv("field_ph.csv",header=TRUE)
+obsCH4<-read.csv("field_ch4.csv",header=TRUE)
+obsCO2<-read.csv("field_co2.csv",header=TRUE)
+obsPOC<-read.csv("field_poc.csv",header=TRUE)
+
+
+
+#compare 2016 modeled TEMP to 2016 obs TEMP
+quartz()
+plot_temp_compare(nc_file = SimFile, obsTEMP)
+
+nc_file=file.path(SimDir, 'output.nc')
+nml_file=file.path(SimDir, 'glm2.nml')
+field_file=file.path(SimDir, 'obsTEMP.csv')
+
+plot_temp_compare(nc_file,field_file)
+
+
+#compare 2016 modeled DO to 2016 obs DO
+quartz()
+plot_var_compare(nc_file = SimFile,juliaDO,var_name = 'DO')
+
+#compare 2016 modeled POC to 2016 obs POC
+quartz()
+plot_var_compare(nc_file = SimFile, juliaPOC, var_name='POC',col_lim = c(0,3))
+
+#compare 2016 modeled CH4 to 2016 obs CH4
+quartz()
+plot_var_compare(nc_file = SimFile, juliaCH4, var_name='CAR_ch4')
+
