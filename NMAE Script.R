ec3c8811afe335bacfbe1c78282ed27144c70bb5
jhart6 <jhart6@wisc.edu>
2017-04-07 21:46:53 -0500

    plotting and model building to get R^2 and spearman's rho values

--- NMAE Script.R
+++ NMAE Script.R
@@ -6,6 +6,7 @@ library(dplyr)
 library(glmtools)
 library(GLMr)
 library(lubridate)
+library(tidyr)
 
 SimDir = '~/Dropbox/LaMe GLM Calibration/Greedy/' 
 setwd(SimDir) 
@@ -27,6 +28,18 @@ glm.doc<-get_var(SimFile, var_name = 'all_DOC', reference = 'surface', z_out = z
 glm.ch4<-get_var(SimFile, var_name = 'CAR_ch4', reference = 'surface', z_out = z_out, t_out = t_out)
 glm.dic<-get_var(SimFile, var_name = 'DIC', reference = 'surface', z_out = z_out, t_out = t_out)
 glm.ph<-get_var(SimFile, var_name = 'CAR_pH', reference = 'surface', z_out = z_out, t_out = t_out)
+
+colnames(glm.temp)<-c('datetime','z.3','z.10','z.12','z.14','z.20')
+colnames(glm.do)<-c('datetime','z.3','z.10','z.12','z.14','z.20')
+colnames(glm.tn)<-c('datetime','z.0','z.4','z.8','z.10','z.12','z.14','z.16','z.18','z.20','z.22')
+colnames(glm.tp)<-c('datetime','z.0','z.4','z.8','z.10','z.12','z.14','z.16','z.18','z.20','z.22')
+colnames(glm.poc)<-c('datetime','z.3','z.10','z.12','z.14','z.20')
+colnames(glm.doc)<-c('datetime','z.3','z.10','z.12','z.14','z.20')
+colnames(glm.ch4)<-c('datetime','z.3','z.10','z.12','z.14','z.20')
+colnames(glm.dic)<-c('datetime','z.3','z.10','z.12','z.14','z.20')
+colnames(glm.ph)<-c('datetime','z.3','z.10','z.12','z.14','z.20')
+
+
 
 #read observed data
 setwd("~/Dropbox/Masters Writing/nmae/")
@@ -136,9 +149,7 @@ hypo.nmae.ph<-nmae_bottom10(glm.ph,obs.ph)
 
 hypo.nmae.all<-c(hypo.nmae.temp,hypo.nmae.do,hypo.nmae.tn,hypo.nmae.tp,hypo.nmae.poc,hypo.nmae.doc,hypo.nmae.ch4,hypo.nmae.dic,hypo.nmae.ph)
 
-#R^2
-library(tidyr)
-
+#convert wide data set to long data set
 gather.obs.data<-function(obs.data,var_name){
   long.data<-gather(obs.data,depth,var_name,z.3:z.20)
   clean.long.data<-long.data %>%
@@ -147,4 +158,123 @@ gather.obs.data<-function(obs.data,var_name){
   return(clean.long.data)
 }
 
+gather.obs.data.nutrients<-function(obs.data,var_name){
+  long.data<-gather(obs.data,depth,var_name,z.0:z.22)
+  clean.long.data<-long.data %>%
+    select(datetime,depth,var_name)
+  colnames(clean.long.data)<-c("datetime",'depth',var_name)
+  return(clean.long.data)
+}
 
+gather.glm.data<-function(glm.data,var_name){
+  long.data<-gather(glm.data,depth,var_name,z.3:z.20)
+  clean.long.data<-long.data %>%
+    select(datetime,depth,var_name)
+  colnames(clean.long.data)<-c("datetime",'depth',var_name)
+  return(clean.long.data)
+}
+
+long.obs.temp<-gather.obs.data(obs.temp,'temp')
+long.obs.do<-gather.obs.data(obs.do,'DO')
+long.obs.tn<-gather.obs.data.nutrients(obs.tn,'TN')
+long.obs.tp<-gather.obs.data.nutrients(obs.tp,'TP')
+long.obs.poc<-gather.obs.data(obs.poc,'POC')
+long.obs.doc<-gather.obs.data(obs.doc,'DOC')
+long.obs.ch4<-gather.obs.data(obs.ch4,'CH4')
+long.obs.dic<-gather.obs.data(obs.dic,'DIC')
+long.obs.ph<-gather.obs.data(obs.ph,'pH')
+
+long.glm.temp<-gather.obs.data(glm.temp,'temp')
+long.glm.do<-gather.obs.data(glm.do,'DO')
+long.glm.tn<-gather.obs.data.nutrients(glm.tn,'TN')
+long.glm.tp<-gather.obs.data.nutrients(glm.tp,'TP')
+long.glm.poc<-gather.obs.data(glm.poc,'POC')
+long.glm.doc<-gather.obs.data(glm.doc,'DOC')
+long.glm.ch4<-gather.obs.data(glm.ch4,'CH4')
+long.glm.dic<-gather.obs.data(glm.dic,'DIC')
+long.glm.ph<-gather.obs.data(glm.ph,'pH')
+
+#R^2 and Spearman Rho
+plot(long.glm.temp$temp,long.obs.temp$temp,pch=16)
+temp.mod<-lm(long.obs.temp$temp~long.glm.temp$temp)
+abline(temp.mod)
+summary(temp.mod)
+cor.test(long.glm.temp$temp,long.obs.temp$temp,method="spearman")
+
+plot(long.glm.do$DO,long.obs.do$DO,pch=16)
+do.mod<-lm(long.obs.do$DO~long.glm.do$DO)
+abline(do.mod)
+summary(do.mod)
+cor.test(long.glm.do$DO,long.obs.do$DO,method='spearman')
+
+plot(long.glm.tn$TN,long.obs.tn$TN)
+tn.mod<-lm(long.obs.tn$TN~long.glm.tn$TN)
+abline(tn.mod)
+summary(tn.mod)
+cor.test(long.glm.tn$TN,long.obs.tn$TN,method='spearman')
+
+plot(long.glm.tp$TP,long.obs.tp$TP)
+tp.mod<-lm(long.obs.tp$TP~long.glm.tp$TP)
+abline(tp.mod)
+summary(tp.mod)
+cor.test(long.glm.tp$TP,long.obs.tp$TP,method='spearman')
+
+plot(long.glm.poc$POC,long.obs.poc$POC)
+poc.mod<-lm(long.obs.poc$POC~long.glm.poc$POC)
+abline(poc.mod)
+summary(poc.mod)
+cor.test(long.glm.poc$POC,long.obs.poc$POC,method='spearman')
+
+plot(long.glm.doc$DOC,long.obs.doc$DOC)
+doc.mod<-lm(long.obs.doc$DOC~long.glm.doc$DOC)
+abline(doc.mod)
+summary(doc.mod)
+cor.test(long.glm.doc$DOC,long.obs.doc$DOC,method='spearman')
+
+plot(long.glm.ch4$CH4,long.obs.ch4$CH4)
+ch4.mod<-lm(long.obs.ch4$CH4~long.glm.ch4$CH4)
+abline(ch4.mod)
+summary(ch4.mod)
+cor.test(long.glm.ch4$CH4,long.obs.ch4$CH4,method='spearman')
+
+plot(long.glm.dic$DIC,long.obs.dic$DIC)
+dic.mod<-lm(long.obs.dic$DIC~long.glm.dic$DIC)
+abline(dic.mod)
+summary(dic.mod)
+cor.test(long.glm.dic$DIC,long.obs.dic$DIC,method='spearman')
+
+plot(long.glm.ph$pH,long.obs.ph$pH)
+ph.mod<-lm(long.obs.ph$pH~long.glm.ph$pH)
+abline(ph.mod)
+summary(ph.mod)
+cor.test(long.glm.ph$pH,long.obs.ph$pH,method='spearman')
+
+quartz()
+par(mfrow=c(3,3))
+plot(long.glm.temp$temp,long.obs.temp$temp,pch=16,xlab='GLM Temp',ylab='Obs Temp')
+abline(temp.mod)
+abline(0,1,col='red')
+plot(long.glm.do$DO,long.obs.do$DO,pch=16,xlab='GLM DO',ylab='Obs DO')
+abline(do.mod)
+abline(0,1,col='red')
+plot(long.glm.tn$TN,long.obs.tn$TN,pch=16,xlab='GLM TN',ylab='Obs TN')
+abline(tn.mod)
+abline(0,1,col='red')
+plot(long.glm.tp$TP,long.obs.tp$TP,pch=16,xlab='GLM TP',ylab='Obs TP')
+abline(tp.mod)
+abline(0,1,col='red')
+plot(long.glm.poc$POC,long.obs.poc$POC,pch=16,xlab='GLM POC',ylab='Obs POC')
+abline(poc.mod)
+abline(0,1,col='red')
+plot(long.glm.doc$DOC,long.obs.doc$DOC,pch=16,xlab='GLM DOC',ylab='Obs POC')
+abline(doc.mod)
+abline(0,1,col='red')
+plot(long.glm.ch4$CH4,long.obs.ch4$CH4,pch=16,xlab=expression(GLM~CH[4]),ylab=expression(Obs~CH[4]))
+abline(ch4.mod)
+abline(0,1,col='red')
+plot(long.glm.dic$DIC,long.obs.dic$DIC,pch=16,xlab='GLM DIC',ylab='Obs DIC')
+abline(dic.mod)
+abline(0,1,col='red')
+plot(long.glm.ph$pH,long.obs.ph$pH,pch=16,xlab='GLM pH',ylab='Obs pH')
+abline(ph.mod)
+abline(0,1,col='red')
