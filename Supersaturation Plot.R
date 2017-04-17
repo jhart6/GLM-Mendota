751cbbad5ed747a00622d93324e0085172895130
jhart6 <jhart6@wisc.edu>
2017-04-17 16:06:18 -0500

    sourced two prior scripts, updated and improved supersaturation plots

--- Supersaturation Plot.R
+++ Supersaturation Plot.R
@@ -1,8 +1,10 @@
 #Supersaturation 1:1 Figure
 #source the Gas Flux script before running this script
+source("~/Dropbox/GitHub Repos/GLM-Mendota/Gas Flux.R")
 
 #start by running Gas Atm Sat script to estimate equilibrium saturation of CO2 and CH4
 #from temperature and pressure
+source("~/Dropbox/GitHub Repos/GLM-Mendota/Gas Atm Sat.R")
 
 ####Calculating CO2 atm saturation####
 obs_henry_co2 <- Kh_Plummer(temp_daily+273.15)
@@ -44,8 +46,32 @@ plot(as.Date(temp$datetime),do.supersat,type='l')
 
 
 ####Create plots####
-plot(do.flux.read.obs,co2.flux.read.obs,pch=16)
-abline(0,-1,lty=2,col='red')
+#supersaturation plot
+quartz()
+par(mar=c(3,3,1,1),mgp=c(1.5,0.5,0),tck=-0.02)
+plot(do.supersat,co2.supersat,type='n',xlim=c(-100,100),ylim=c(-100,100),ylab=expression((CO[2][Obs]-CO[2][Eq])),xlab=expression((DO[Obs]-DO[Eq])))
+abline(h=0,col='red',lty=2,lwd=2)
+abline(v=0,col='red',lty=2,lwd=2)
+points(do.supersat,co2.supersat,pch=16)
 
-plot(do.supersat,co2.supersat,pch=16)
-abline(0,-1,lty=2,col='red')
+
+#flux comparison plot
+setwd("~/Dropbox/GitHub Repos/GLM-Mendota/Data/")
+flux<-read.csv('flux.data.csv')
+quartz()
+plot(do.flux.read.obs,co2.flux.read.obs.do,pch=16,xlim=c(-200,200),ylim=c(-200,200))
+abline(0,-1,col='red')
+
+xlab=expression(Observed~DO~Flux~(mmol~m^-2~day^-1))
+ylab.do=expression(Biological~CO[2]~Flux~(mmol~m^-2~day^-1))
+ylab.obs=expression(Observed~CO[2]~Flux~(mmol~m^-2~day^-1))
+
+quartz()
+par(mfrow=c(1,2),mar=c(3,3,1,1),mgp=c(1.5,0.5,0),tck=-0.02)
+plot(do.flux.read.obs,co2.flux.read.obs.do,pch=16,xlim=c(-200,300),ylim=c(-200,300),xlab=xlab,ylab=ylab.do)
+abline(0,-1,col='red',lwd=2)
+plot(do.flux.read.obs,co2.flux.read.obs,pch=16,ylim=c(-200,300),xlim=c(-200,300),xlab=xlab,ylab=ylab.obs)
+abline(0,-1,lwd=2,col='red')
+
+
+
