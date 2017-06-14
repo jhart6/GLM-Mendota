630854bbc1ae2028cc87cf163aa701fe7ccd6086
jhart6 <jhart6@wisc.edu>
2017-06-14 13:11:15 -0500

    option for plotting function with legend in topleft

--- P Speciation.R
+++ P Speciation.R
@@ -18,3 +18,25 @@ PSpecies <- function(SimFile,Depth){
   myLegend = c(Vars2Plot,'Phytos are dashed')
   legend("topright",y=NULL,myLegend,fill = ColorSch)
 }
+
+
+
+PSpecies <- function(SimFile,Depth){
+  myZ = Depth
+  myYlabel = expression(P~Speciation~(mmol~m^-3))
+  Vars2Plot = c('TOT_tp','OGM_pop','OGM_dop','PHS_frp','PHS_frp_ads','PHY_CYANOPCH1_IP',
+                'PHY_CYANONPCH2_IP','PHY_CHLOROPCH3_IP','PHY_DIATOMPCH4_IP')
+  ColorSch = c('black','green','blue','red','grey','blue','red','green','black','white')
+  LineType = c(1,1,1,1,1,2,2,2,2)
+  
+  Total<-get_var(SimFile,var_name = Vars2Plot[1],z_out = myZ,reference = 'surface')
+  myYlim = c(0,max(Total[,2])*1.2)
+  plot(Total,type='l',ylim=myYlim,col=ColorSch[1],lwd=3,ylab = myYlabel)
+  for (i in 2:length(Vars2Plot)){
+    lines(get_var(SimFile,var_name = Vars2Plot[i],z_out = myZ,reference = 'surface'),
+          col=ColorSch[i],lty=LineType[i])
+  }
+  myLegend = c(Vars2Plot,'Phytos are dashed')
+  legend("topleft",y=NULL,myLegend,fill = ColorSch)
+}
+
