d44c6dbfdbe69f79047d784ef6fcbe4988f48a9d
jhart6 <jhart6@wisc.edu>
2017-06-22 16:50:39 -0500

    code to create observed data panel using rainbow plot_field_obspanel (OG + collim argument)

---
+++ Obs Data Panel with Plotfield.R
@@ -0,0 +1,17 @@
+#Use plot_field_obspanel function
+  #original formatting for large plot
+  #includes collim argument
+
+setwd('~/Dropbox/LaMe GLM Calibration/Observed Data/')
+
+png('obspoc.png',width=7,height=2.5,units='in',res=300)
+plot_field_obspanel('field_poc.csv','POC','mg/L',collim=c(0,3))
+dev.off()
+
+png('obsch4.png',width=7,height=2.5,units='in',res=300)
+plot_field_obspanel('field_log_ch4.csv','log(CH4)','umol/L',collim=c(-3,3))
+dev.off()
+
+png('obsco2.png',width=7,height=2.5,units='in',res=300)
+plot_field_obspanel('field_log_co2.csv','log(CO2)','umol/L')
+dev.off()
