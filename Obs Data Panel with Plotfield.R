542038c9a244c082ba0ee59d3e52349b54978614
jhart6 <jhart6@wisc.edu>
2017-07-05 15:38:09 -0500

    added xlim to obs data panel plots

--- Obs Data Panel with Plotfield.R
+++ Obs Data Panel with Plotfield.R
@@ -9,9 +9,9 @@ plot_field_obspanel('field_poc.csv','POC','mg/L',collim=c(0,3))
 dev.off()
 
 png('obsch4.png',width=7,height=2.5,units='in',res=300)
-plot_field_obspanel('field_log_ch4.csv','log(CH4)','umol/L',collim=c(-3,3))
+plot_field_obspanel('field_log_ch4.csv','log(CH4)','umol/L',collim=c(-3,3),xlims=as.Date(c('2016-04-15','2016-11-14')))
 dev.off()
 
 png('obsco2.png',width=7,height=2.5,units='in',res=300)
-plot_field_obspanel('field_log_co2.csv','log(CO2)','umol/L')
+plot_field_obspanel('field_log_co2.csv','log(CO2)','umol/L',xlims=as.Date(c('2016-04-15','2016-11-14')))
 dev.off()
